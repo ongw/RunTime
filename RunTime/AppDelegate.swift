0b15623c97fb2384d0bbb281fc670b0c93615919
albertkim96 <albertkim1996@gmail.com>
2017-09-16 18:06:31 -0400

    Update AppDelegate.swift

--- RunTime/AppDelegate.swift
+++ RunTime/AppDelegate.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import Stripe
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        STPPaymentConfiguration.shared().publishableKey = "sk_test_eTGbY1rYDxYCLWQP6f5tsQfb"
+        // do any other necessary launch configuration
+        print ("hello")
         return true
     }
 
@@ -40,7 +42,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
